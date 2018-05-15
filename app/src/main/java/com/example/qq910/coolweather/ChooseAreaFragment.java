fc4bf5fa18706c62bc88404dbab37a954582d8cb
liuzihao <910554351@qq.com>
2018-05-16 00:59:39 +0800

    完成遍历省市县三级列表的功能.

---
+++ app/src/main/java/com/example/qq910/coolweather/ChooseAreaFragment.java
@@ -0,0 +1,8 @@
+package com.example.qq910.coolweather;
+
+/**
+ * Created by qq910 on 2018/5/15.
+ */
+
+public class ChooseAreaFragment {
+}
---
+++ app/src/main/java/com/example/qq910/coolweather/util/Utillty.java
@@ -0,0 +1,8 @@
+package com.example.qq910.coolweather.util;
+
+/**
+ * Created by qq910 on 2018/5/15.
+ */
+
+public class Utillty {
+}
